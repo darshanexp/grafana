4dfced7428dfcb5ea7989a462a93a0202bd04760
agokmen <agokmen@riotgames.com>
2018-04-03 13:18:36 -0700

    use env proxy on oauth http client

--- pkg/api/login_oauth.go
+++ pkg/api/login_oauth.go
@@ -86,6 +86,7 @@ func OAuthLogin(ctx *m.ReqContext) {
 
 	// handle call back
 	tr := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
 		TLSClientConfig: &tls.Config{
 			InsecureSkipVerify: setting.OAuthService.OAuthInfos[name].TlsSkipVerify,
 		},
